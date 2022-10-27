f26a5a5993a29061f49a994fe4fa7813e7ec961e
talip <talip@zir-ai.com>
2022-10-26 23:19:31 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,71 @@
+# Readme
+
+The source code in this repository shows a simple example of how to build
+semantic search applications with Vectara Semantic Search.
+
+## Data
+
+`/src/main/resources`, Contains customer reviews for three San Francisco hotels.
+
+## Programs
+
+## opinrank2json.py
+
+This program converts reviews from Opinrank's tab-separated into documents that
+can be dragged and dropped into the Vectara Console. It also saves the documents in
+a SQLite database which is used when running queries.
+
+```bash
+declare -a files=(
+  "usa_san_francisco_best_western_tuscan_inn_fisherman_s_wharf_a_kimpton_hotel"
+  "usa_san_francisco_sheraton_fisherman_s_wharf_hotel"
+  "usa_san_francisco_the_westin_st_francis")
+
+for FILE in "${files[@]}"
+do
+  ./src/main/opinrank2json.py\
+      --input-file src/main/resources/${FILE}\
+      --output-dir ./out\
+      --sqlite ./out/reviews.db
+done
+```
+
+## hotels.py
+
+This program provides a line-oriented interpreter for running queries against a
+corpus of hotel reviews. Here's a sample session:
+
+```
+$ ./src/main/hotels.py\
+    --customer-id 1526022105\
+    --corpus-id 14\
+    --app-client-id 20hestglli47sd563mbq01ha9e\
+    --auth-domain https://vectara-prod-1526022105.auth.us-west-2.amazoncognito.com
+App Client Secret:
+Welcome to hotel search. Ask a question and get an answer.
+(hotel) are the rooms spacious?
+
+Very Nice --Would recommend to my friends
+in a convenient location. The Tuscan Inn fit that well. Very
+comfortable stay and got a good deal on Orbitz to boot. Only drawback
+is room size. We started w/ a king w/ sofa bed for my son but we were
+tripping on each other so we upgraded to a suite. We also moved from a
+Best Western Fishermans Wharf reviewed on Apr 12 2008
+
+Loved the Tuscan Inn
+We did it all. Hotel was Very nicely furnished, staff was Very
+helpful. The rooms are just a tad bit small---but we didn't spend much
+time in our room. Very well cleaned. I will continue to recommend this
+property.
+Best Western Fishermans Wharf reviewed on Jan 10 2007
+
+Fun Hotel in the Heart of Everything
+staff was very nice. The lobby has a good vibe. We had a room on the
+top floor with dormer windows. I would request these rooms, because
+they feel bigger. The bathroom was standard issue on the small side.
+We really enjoyed the pool. It is hard to find an outdoor pool in
+Sheraton Fisherman's Wharf Hotel reviewed on Jul 15 2009
+
+
+Returned 3 results in 0.213 secs.
+```
---
+++ src/main/opinrank2json.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+"""Parse OpinRank hotel reviews and save each review as a JSON document."""
+
+import argparse
+import json
+import logging
+import os
+import sqlite3
+import sys
+
+import mmh3
+
+def eprint(*args, **kwargs):
+    """Print to stderr.
+
+    Ref: https://stackoverflow.com/questions/5574702/how-to-print-to-stderr-in-python
+    """
+    print(*args, file=sys.stderr, **kwargs)
+
+def main(args):
+    if not os.path.isfile(args.input_file):
+        eprint(f"{args.input_file} does not exist.")
+        sys.exit(1)
+
+    con = sqlite3.connect(args.sqlite_out)
+    cur = con.cursor()
+    init_db(cur)
+
+    with open(args.input_file, encoding="utf8", errors="ignore") as infile:
+        num_written = 0
+        for line in infile:
+            parts = line.split("\t")
+
+            doc_id = mmh3.hash128(line, 42, True)  # True is the x64 version
+            date, title, text = parts[0], parts[1], parts[2]
+            if not text:  # Skip empty reviews
+                continue
+            num_written += 1
+
+            hotel = os.path.basename(args.input_file)
+            if hotel.startswith("usa_san_francisco"):
+                hotel = hotel[18:]
+
+            json_doc = {
+                "documentId": str(doc_id),
+                "title": title,
+                "metadataJson": '{"date": "%s", "hotel": "%s"}' % (date, hotel),
+                "section": [
+                    { "text": text }
+                ]
+            }
+            doc = json.dumps(json_doc, sort_keys=True, indent=4)
+
+            outfile = os.path.join(args.output_dir, str(doc_id)) + ".json"
+            with open(outfile, 'w') as outfile:
+                outfile.write(doc)
+
+            cur.execute("insert or ignore into reviews values (?, ?, ?, ?, ?)",
+                        (str(doc_id), title, date, hotel, text))
+
+        print(f"Wrote {num_written} reviews to {args.output_dir} "
+              f"and {args.sqlite_out}.")
+        con.commit()
+        con.close()
+
+
+def init_db(cur):
+    """Create the reviews table in the database."""
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS reviews (
+            doc_id text primary key,
+            title text,
+            date text,
+            hotel text,
+            review text)
+    ''')
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Extract OpinRank hotel reviews.")
+
+    parser.add_argument("--input-file", dest="input_file",
+                        help="Location of the input file.",
+                        metavar="FILE")
+    parser.add_argument("--output-dir", dest="output_dir",
+                        default="./out",
+                        help="Location where hotel reviews will be exported as JSON.",
+                        metavar="DIR")
+    parser.add_argument("--sqlite", dest="sqlite_out",
+                        default="./out/reviews.db",
+                        help="Database file for hotel reviews.",
+                        metavar="FILE")
+    args = parser.parse_args()
+
+    root = logging.getLogger()
+    root.setLevel(logging.DEBUG)
+
+    logging.basicConfig(
+        format="%(asctime)s %(thread)-4d:%(levelname)-8s %(message)s",
+        level=logging.INFO)
+    main(args)
